942b0d9a25f94faa6a59ae85e379c562f886066c
Bégin <gbegin@axso.io>
2025-09-26 15:37:53 -0400

    update nvim config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,9 +6,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
+
 vim.opt.swapfile = false
 vim.opt.backup = false
-
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
@@ -17,7 +17,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.updatetime = '50'
+vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = '80'
 
