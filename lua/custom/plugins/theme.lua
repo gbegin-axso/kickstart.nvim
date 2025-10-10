58d099839ca70d40676e4baa30972f484a3a864c
Bégin <gbegin@axso.io>
2025-10-10 14:00:28 -0400

    update theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,5 +1,6 @@
 return {
   {
+    enabled = false,
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
@@ -7,6 +8,25 @@ return {
         style = 'darker',
       }
       require('onedark').load()
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000, -- ensures it loads early
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'latte', -- choose latte flavor
+        background = {
+          light = 'latte',
+          dark = 'mocha', -- fallback dark theme
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        -- Add additional configs if needed
+      }
+      -- Apply the colorscheme after setup
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 }
