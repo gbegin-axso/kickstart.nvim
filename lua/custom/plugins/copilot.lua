8a4ae3aa455dee10c7261a81163c788e30adf5f4
Bégin <gbegin@axso.io>
2025-10-10 08:07:42 -0400

    add dependecy (copilot-cmp)

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -62,6 +62,14 @@ return {
       'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
+      {
+        'zbirenbaum/copilot-cmp',
+        event = 'InsertEnter',
+        dependencies = { 'zbirenbaum/copilot.lua' },
+        config = function()
+          require('copilot_cmp').setup()
+        end,
+      },
       --- The below dependencies are optional,
       'echasnovski/mini.pick', -- for file_selector provider mini.pick
       'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
