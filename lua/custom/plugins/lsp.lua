32f0b9dba51a9a4e43749cb92900e6ba9598118b
Bégin <gbegin@axso.io>
2025-10-15 08:24:08 -0400

    remove double typescript lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,10 +1,5 @@
 return {
   {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-  },
-  {
     'ray-x/go.nvim',
     dependencies = { -- optional packages
       'ray-x/guihua.lua',
