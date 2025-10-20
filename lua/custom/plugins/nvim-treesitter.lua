fd1a4fc7586e1096d2f002dee2ab0bb923e3db9b
Bégin <gbegin@axso.io>
2025-10-20 10:32:13 -0400

    add treesitter-context, unable autosave and disable format on save

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -1,5 +1,15 @@
 return {
   {
-    'nvim-neo-tree/neo-tree.nvim',
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup {
+        -- Optionally add your custom configuration here,
+        -- For example, enable the plugin or set max_lines, separator, etc.
+        enable = true,
+        max_lines = 0,
+        line_numbers = true,
+        mode = 'cursor',
+      }
+    end,
   },
 }
