a85d0af4ca2c5b92f5b8a1307e18c425d5ada65a
Bégin <gbegin@axso.io>
2025-10-23 11:03:43 -0400

    add relativenumber

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,6 +7,9 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
