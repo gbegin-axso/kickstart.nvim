2f3c6d062b6e4cf1b7baff00911f959624ab021e
Guillaume Bégin <gbegin@axso.io>
2025-12-05 09:30:02 -0500

    ,

--- lua/custom/plugins/guess-indent.lua
+++ lua/custom/plugins/guess-indent.lua
@@ -1,7 +1,10 @@
 return {
   "nmac427/guess-indent.nvim",
-  event = "BufReadPre",  -- or "VeryLazy"
+  event = "BufReadPre",
   config = function()
-    require("guess-indent").setup({})
+    require("guess-indent").setup({
+      auto_cmd = true,          -- run on BufRead
+      override_editorconfig = false,
+    })
   end,
 }
