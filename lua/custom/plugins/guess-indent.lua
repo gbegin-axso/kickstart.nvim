d0d57746b0d000d60e554007da2dad7e18e43617
Guillaume Bégin <gbegin@axso.io>
2025-12-05 09:27:58 -0500

    require guest-ident

--- lua/custom/plugins/guess-indent.lua
+++ lua/custom/plugins/guess-indent.lua
@@ -1,5 +1,7 @@
 return {
-  {
-    'nmac427/guess-indent.nvim',
-  },
+  "nmac427/guess-indent.nvim",
+  event = "BufReadPre",  -- or "VeryLazy"
+  config = function()
+    require("guess-indent").setup({})
+  end,
 }
