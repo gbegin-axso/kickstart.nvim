3ebedaf4110230b335f849305e3783146b78b14f
Guillaume Bégin <gbegin@axso.io>
2025-12-05 09:09:23 -0500

    remove auto-indent

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,7 +34,6 @@ vim.opt.foldenable = false
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = 'indent'
 
-vim.opt_local.autoindent = true
 vim.opt_local.smartindent = true
 
 require 'custom.plugins.theme'
