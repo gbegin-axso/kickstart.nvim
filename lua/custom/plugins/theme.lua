dbd11fc67c3f3ec8dfc7b9cfff6fff7390ada8d6
Guillaume Bégin <gbegin@axso.io>
2026-01-15 07:56:13 -0500

    change theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,50 +1,19 @@
--- Toggle between light and dark Catppuccin themes
-local current_flavour = 'mocha'
+-- -- Toggle between light and dark Catppuccin themes
+-- local current_flavour = 'mocha'
 
-local function toggle_catppuccin()
-  if current_flavour == 'mocha' then
-    current_flavour = 'latte'
-  else
-    current_flavour = 'mocha'
-  end
-  require('catppuccin').setup { flavour = current_flavour }
-  vim.cmd.colorscheme 'catppuccin'
-end
+-- local function toggle_catppuccin()
+--   if current_flavour == 'mocha' then
+--     current_flavour = 'latte'
+--   else
+--     current_flavour = 'mocha'
+--   end
+--   require('catppuccin').setup { flavour = current_flavour }
+--   vim.cmd.colorscheme 'catppuccin'
+-- end
 
-vim.keymap.set('n', '<leader>tc', toggle_catppuccin, { desc = '[T]oggle [C]atppuccin theme', noremap = true, silent = true })
+-- vim.keymap.set('n', '<leader>tc', toggle_catppuccin, { desc = '[T]oggle [C]atppuccin theme', noremap = true, silent = true })
 
 return {
-  {
-    enabled = false,
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      require('onedark').setup {
-        style = 'darker',
-      }
-      require('onedark').load()
-    end,
-  },
-  {
-    enabled = true,
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000, -- ensures it loads early
-    config = function()
-      require('catppuccin').setup {
-        flavour = 'mocha',
-        background = {
-          light = 'latte',
-          dark = 'mocha', -- fallback dark theme
-        },
-        transparent_background = false,
-        show_end_of_buffer = false,
-        -- Add additional configs if needed
-      }
-      -- Apply the colorscheme after setup
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
   {
     enabled = false,
     'loctvl842/monokai-pro.nvim',
