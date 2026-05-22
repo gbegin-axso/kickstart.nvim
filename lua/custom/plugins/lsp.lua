e199bfdba1d7d9fc379608e88ef890303378d4fb
Guillaume Bégin <gbegin@axso.io>
2026-05-22 10:57:23 -0400

    add c# lsp

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -26,4 +26,27 @@ return {
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
   { 'codenamedevan/pinescriptv6' },
+  {
+    'seblyng/roslyn.nvim',
+    ft = 'cs',
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+    config = function(_, opts)
+      require('roslyn').setup(opts)
+      vim.lsp.config('roslyn', {
+        settings = {
+          ['csharp|inlay_hints'] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+          },
+          ['csharp|code_lens'] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      })
+    end,
+  },
 }
